f1e881c771d9ad0a14c469289640f56fa201de3c
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:54:49 -0500

    Update form.php

--- form.php
+++ form.php
@@ -65,20 +65,20 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
+<p>PHP Form Validation Example</p>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name">
   <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
+  <br>
   E-mail: <input type="text" name="email">
   <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
+  <br>
   Website: <input type="text" name="website">
   <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
+  <br>
   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-  <br><br>
+  <br>
   Gender:
   <input type="radio" name="gender" value="female">Female
   <input type="radio" name="gender" value="male">Male
@@ -89,7 +89,7 @@ function test_input($data) {
 </form>
 
 <?php
-echo "<h2>Your Input:</h2>";
+echo "<p>Your Input:</p>";
 echo $name;
 echo "<br>";
 echo $email;
