6938f0587ddfec65cf4135c7c115c23427e9a95b
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:17:12 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
 <div class="footer">
-  <?php
-date_default_timezone_set("America/New_York");
-echo "The time is " . date("h:i:sa");
-?>
+
 <?php
 echo "The time is " . date("h:i:sa");
 ?>
+  
+  
+<?php
+$d1=strtotime("December 25");
+$d2=ceil(($d1-time())/60/60/24);
+echo "There are " . $d2 ." days until Christmas!";
+?>
+  
 </div>
