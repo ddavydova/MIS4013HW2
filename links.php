db42066e540a102993edcd570b0d7e7d9222096a
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:48:48 -0500

    Update links.php

--- links.php
+++ links.php
@@ -1,7 +1,7 @@
 <div class="container lead">
 	<ul class="nav nav-pills nav-fill">
           <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="home.php">Home</a>
 	  </li>
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Form") {?>active<?php }?>" href="form.php">Form</a>
