1b4f4b74326d53b9830215f9e5f2bda8c37e0022
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 14:30:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 
-<?php include("includes/a_config.php");?>
+<?php include("a_config.php");?>
 <!DOCTYPE html>
 <html>
 <head>
-	<?php include("includes/head-tag-contents.php");?>
+	<?php include("head-tag-contents.php");?>
 </head>
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+<?php include("design-top.php");?>
+<?php include("navigation.php");?>
 
 <div class="container" id="main-content">
 	<h2>Welcome to my website!</h2>
@@ -22,7 +22,7 @@
 	</p>
 </div>
 
-<?php include("includes/footer.php");?>
+<?php include("footer.php");?>
 
 </body>
 </html>
