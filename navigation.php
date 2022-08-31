c67f04e7f91941033fe31a822ec52a86ee8a6b51
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:44:17 -0500

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <div class="container lead">
-	<ul class="nav nav-pills">
-	  <li class="nav-item">
+	<ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
 	  <li class="nav-item">
