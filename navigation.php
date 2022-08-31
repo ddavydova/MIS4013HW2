2b241478dbea20542546808e1ff85594dd72ce38
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:09:08 -0500

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -8,8 +8,5 @@
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
 	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact</a>
-	  </li>
 	</ul>
 </div>
