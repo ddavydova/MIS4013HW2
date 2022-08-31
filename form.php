30408d17d61eeb1a49d8e847b54027c64ebe66a5
Dasha Davydova <dasha.davydova@ou.edu>
2022-08-31 18:55:53 -0500

    Update form.php

--- form.php
+++ form.php
@@ -70,15 +70,15 @@ function test_input($data) {
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name">
   <span class="error">* <?php echo $nameErr;?></span>
-  <br>
+  <br><br>
   E-mail: <input type="text" name="email">
   <span class="error">* <?php echo $emailErr;?></span>
-  <br>
+  <br><br>
   Website: <input type="text" name="website">
   <span class="error"><?php echo $websiteErr;?></span>
-  <br>
+  <br><br>
   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-  <br>
+  <br><br>
   Gender:
   <input type="radio" name="gender" value="female">Female
   <input type="radio" name="gender" value="male">Male
