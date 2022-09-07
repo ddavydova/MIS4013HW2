ad8b814ddfb251ba4af256126cb779cf0e800da0
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-07 13:54:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,6 @@
 
 <div class="container display-5">
 
-  <?php
-   if( $_GET["name"] || $_GET["age"] ) {
-      echo "Welcome ". $_GET['name']. "<br />";
-      echo "You are ". $_GET['age']. " years old.";
-      
-      exit();
-   }
-?>
 <html>
    <body>
    
