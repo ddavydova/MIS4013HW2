fe1f519f73df5bce202933f49b0467b2be701423
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-07 13:56:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,13 @@
 
 <div class="container display-5">
 
-<html>
-   <body>
-   <?php include("getfunction.php");?>
       <form action = "<?php $_PHP_SELF ?>" method = "GET">
          Name: <input type = "text" name = "name" />
          Age: <input type = "text" name = "age" />
          <input type = "submit" />
       </form>
       
-   </body>
-</html>
+     <?php include("getfunction.php");?>
 
 </div>
 
