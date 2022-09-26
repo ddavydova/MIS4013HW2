d2a73b0662897a99d3dccf71fb92d94f5763a43f
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-26 13:59:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
       <?php include("getfunction.php");?>
       </div>
 
+<form action="welcome.php" method="get">
+Name: <input type="text" name="name" />
+Age: <input type="text" name="age" />
+<input type="submit" />
+</form> 
+Welcome <?php echo $_GET["name"]; ?>.<br />
+You are <?php echo $_GET["age"]; ?> years old!
+
 <br>
 <?php include("footer.php");?>
 
