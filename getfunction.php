82462517f18112458806e1d16feb753c8d3d19b0
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-26 13:37:33 -0500

    Update getfunction.php

--- getfunction.php
+++ getfunction.php
@@ -1,24 +1,12 @@
-
 <?php
-   if( $_POST["name"] || $_POST["age"] ) {
-      if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
-         die ("invalid name and name should be alpha");
-      }
-      echo "Welcome ". $_POST['name']. "<br />";
-      echo "You are ". $_POST['age']. " years old.";
-      
-      exit();
-   }
-?>
-<html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "POST">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         <input type = "submit" />
-      </form>
-  
 
-</body>
+$bg_color = htmlspecialchars($_GET['bg']);
+$text = htmlspecialchars($_GET['text']);
+
+?>
+
+<html>
+  <body style="background: <?php echo $bg_color ?>">
+    <h1><?php echo $text ?></h1>
+  </body>
 </html>
